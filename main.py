f1a0ed8b2b9735c21ae76e3f98ace0871b56f5b5
Yogarajalakshmi <yogarajalakshmis@gmail.com>
2023-05-23 13:02:18 +0530

    pack, place and grid layout managers, padding, other additional tkinter widgets

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 from tkinter import *
 
+
+def button_click():
+    my_label['text'] = entry.get()  # gets entry input
+
+
 window = Tk()
 window.title('My First GUI Program')
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)  # Adding padding to the entire screen
 
 # Label component
 my_label = Label(text="I am a label", font=('Arial', 24, 'bold'))
-my_label.pack()
-my_label['text'] = 'New Text 1'  # Changing the kwarg 'text'
-my_label.config(text='New Text 2')
+# Pack, Place and Grid examples - Layouts
+# Note: We can't mix up pack(). place() and grid() for different components
+# my_label.pack(side='left')
+# my_label.place(x=0, y=0)
+my_label.grid(column=0, row=0)  # Grid is highly used for layouts
+my_label.config(padx=50, pady=50)
 
+# my_label['text'] = 'New Text 1'  # Changing the kwarg 'text'
+# my_label.config(text='New Text 2')
 
-# Button
-def button_click():
-    my_label['text'] = 'Clicked!!'
+# Button component
+button_1 = Button(text='Click Me!', command=button_click)
+# button.pack()
+button_1.grid(column=1, row=1)
 
+button_2 = Button(text='Click Me!', command=button_click)
+# button.pack()
+button_2.grid(column=2, row=0)
 
-button = Button(text='Click Me!', command=button_click)
-button.pack()
+# Entry component - We can give our input in this box
+entry = Entry(width=30)
+entry.insert(END, string='Some text to begin with')  # Inserts a default text to the entry
+# entry.pack()
+entry.grid(column=3, row=2)
 
 window.mainloop()
---
+++ tkinter_additional_widgets.py
@@ -0,0 +1,67 @@
+from tkinter import *
+
+window = Tk()
+window.title('My First GUI Program')
+window.minsize(width=500, height=300)
+
+# Text
+text = Text(width=30, height=5)
+text.focus()  # Puts cursor in the textbox
+text.insert(END, 'Example of multi-line string entry')
+print(text.get('1.0', END))
+text.pack()
+
+
+# Spinbox
+def spinbox_used():
+    print(spinbox.get())
+
+
+spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
+spinbox.pack()
+
+
+# Scale
+def scale_used(value):  # Called with current scale value
+    print(value)
+
+
+scale = Scale(from_=0, to=100, command=scale_used)
+scale.pack()
+
+
+# Checkbutton
+def checkbutton_used():
+    print(checked_state.get())  # Prints 1 if On button checked, otherwise 0.
+
+
+checked_state = IntVar()  # variable to hold on to checked state, 0 is off, 1 is on.
+checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
+checkbutton.pack()
+
+
+# Radiobutton
+def radio_used():
+    print(radio_state.get())
+
+
+radio_state = IntVar()  # Variable to hold on to which radio button value is checked.
+radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
+radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+radiobutton1.pack()
+radiobutton2.pack()
+
+
+# Listbox
+def listbox_used(event):
+    print(listbox.get(listbox.curselection()))  # Gets current selection from listbox
+
+
+listbox = Listbox(height=4)
+fruits = ["Apple", "Pear", "Orange", "Banana"]
+for item in fruits:
+    listbox.insert(fruits.index(item), item)
+listbox.bind("<<ListboxSelect>>", listbox_used)
+listbox.pack()
+
+window.mainloop()
