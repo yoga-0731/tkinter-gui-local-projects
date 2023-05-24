36ab2c914f52381f1eafcf8cf9e628f72e9f61eb
Yogarajalakshmi <yogarajalakshmis@gmail.com>
2023-05-24 11:15:50 +0530

    pomodoro app using tkinter

--- main.py
+++ main.py
@@ -1,40 +1,82 @@
+import math
 from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+timer_cycle = 0
+timer = None
 
 
-def button_click():
-    my_label['text'] = entry.get()  # gets entry input
+# ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    global timer_cycle
+    timer_cycle = 0
+    window.after_cancel(timer)
+    timer_label.config(text='Timer', fg=GREEN)
+    canvas.itemconfig(timer_text, text="00:00")
+    checkmark.config(text="")
 
 
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global timer_cycle
+    timer_cycle += 1
+    if timer_cycle % 8 == 0:
+        timer_label.config(text='Break!', fg=RED)
+        count_down(LONG_BREAK_MIN * 60)
+    elif timer_cycle % 2 == 0:
+        timer_label.config(text='Break!', fg=PINK)
+        count_down(SHORT_BREAK_MIN * 60)
+    else:
+        timer_label.config(text='Work!', fg=GREEN)
+        count_down(WORK_MIN * 60)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_minute = math.floor(count / 60)
+    count_second = count % 60
+    if count_second < 10:
+        count_second = f"0{count_second}"  # Dynamic Typing (count_sec previously had int, now has str)
+    canvas.itemconfig(timer_text, text=f"{count_minute}:{count_second}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        for _ in range(math.floor(timer_cycle/2)):
+            mark += "✔"
+        checkmark.config(text=mark)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
-window.title('My First GUI Program')
-window.minsize(width=500, height=300)
-window.config(padx=20, pady=20)  # Adding padding to the entire screen
+window.config(padx=100, pady=50, bg=YELLOW)
+window.title('Pomodoro')
 
-# Label component
-my_label = Label(text="I am a label", font=('Arial', 24, 'bold'))
-# Pack, Place and Grid examples - Layouts
-# Note: We can't mix up pack(). place() and grid() for different components
-# my_label.pack(side='left')
-# my_label.place(x=0, y=0)
-my_label.grid(column=0, row=0)  # Grid is highly used for layouts
-my_label.config(padx=50, pady=50)
+timer_label = Label(text='Timer', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30, 'bold'))
+timer_label.grid(row=0, column=1)
 
-# my_label['text'] = 'New Text 1'  # Changing the kwarg 'text'
-# my_label.config(text='New Text 2')
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)  # The image's width and height
+tomato = PhotoImage(file='tomato.png')
+canvas.create_image(100, 112, image=tomato)  # Half of image's width and height
+timer_text = canvas.create_text(102, 130, text="00:00", fill='white', font=(FONT_NAME, 25, 'bold'))
+canvas.grid(row=1, column=1)
 
-# Button component
-button_1 = Button(text='Click Me!', command=button_click)
-# button.pack()
-button_1.grid(column=1, row=1)
+start_button = Button(text='Start', bg='white', command=start_timer)
+start_button.grid(row=2, column=0)
 
-button_2 = Button(text='Click Me!', command=button_click)
-# button.pack()
-button_2.grid(column=2, row=0)
+reset_button = Button(text='Reset', bg='white', command=reset_timer)
+reset_button.grid(row=2, column=2)
 
-# Entry component - We can give our input in this box
-entry = Entry(width=30)
-entry.insert(END, string='Some text to begin with')  # Inserts a default text to the entry
-# entry.pack()
-entry.grid(column=3, row=2)
+checkmark = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 10, 'bold'))
+checkmark.grid(row=3, column=1)
 
 window.mainloop()
